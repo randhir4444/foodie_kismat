27d13ec58cf703ba70d0219a6367a3c41536a846
Randhir <randhir4444@gmail.com>
2021-03-14 22:34:38 +0530

    Second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router';
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Services from "./Services";
+import NavBar from "./NavBar";
+import Popper from 'popper.js';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "./index.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <NavBar/>
+            <Switch>
+            <Route path="/" exact component={Home}/>
+            <Route path="/About" exact component={About}/>
+            <Route path="/Contact" exact component={Contact}/>
+            <Route path="/Services" exact component={Services}/>
+            <Redirect to="/"/>
+            </Switch>
+        </div>
+    )
 }
 
 export default App;
---
+++ src/Services.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Services() {
+    return (
+        <div>
+            <h1>Hey i am Services page</h1>
+        </div>
+    )
+}
+
+export default Services;
