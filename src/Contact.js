7ec7d3af32ba094fda97ac419f20ce2363f34109
Randhir <randhir4444@gmail.com>
2021-03-16 21:39:00 +0530

    16_mar

--- src/Contact.js
+++ src/Contact.js
@@ -3,17 +3,27 @@ import contactus from "./image/contactus.png";
 import Common from "./Common";
 
 function Contact() {
-    return (
-        <div>
-            <Common 
-                start="Contact us"
-                name="Feel free to contact us anytime."
-                des="We're fulfilling this mission by improving life in the kitchen
-                    for millions of home cooks around the world."
-                btn="Contact us"
-                img={contactus}
-            />
+    return (<>
+        <div className="text-center">
+            <h1>Contact Us</h1>
         </div>
+        <div className="container contact_div">
+            <div className="row">
+                <div className="col-md-6 col-10 mx-auto">
+
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </>
     )
 }
 
